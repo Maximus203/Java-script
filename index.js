94f86191ffc6b56c18626e59e9d7aa60ea92afbe
Maximus203 <printf0cherif@gmail.com>
2022-08-11 11:43:29 +0000

    003-exemple

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-let inputText = document.getElementById('inputText');
+let resultat = document.getElementsByClassName('resultat')[0];
+let h = 10, m = 55, s = 23;
 
-inputText.addEventListener('change', (event) => {
-    let resultat = document.getElementsByClassName('resultat')[0];
-    resultat.innerHTML = `
-    <h3>Vous avez ecrit: ${event.target.value}</h3><br>
-    <h3>Nombre de lettre saisie: ${event.target.value.length}</h3>
-    <h3>Votre saisie en majuscule: ${event.target.value.toUpperCase()}</h3>
-    `;
-});
+setInterval(() => {
+    if (s < 59)
+        s++;
+    else {
+        s = 0;
+        if (m < 59)
+            m++;
+        else {
+            m = 0;
+            if (h < 23)
+                h++;
+            else
+                h = 0;
+        }
+    }
+
+    h.length == 1 ? h = `0${h}` : h = `${h}`;
+    m.length == 1 ? m = `0${m}` : m = `${m}`;
+    s.length == 1 ? s = `0${s}` : s = `${s}`;
+
+    resultat.innerHTML = `${h}h: ${m}m: ${s}s`;
+}, 1000);
