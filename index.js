de313d5ce590cdc23b7ab71a46bedf65fc1ad374
Maximus203 <printf0cherif@gmail.com>
2022-08-11 15:41:56 +0000

    004-exemple

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-let resultat = document.getElementsByClassName('resultat')[0];
-let h = 10, m = 55, s = 23;
+let myCheckbok = document.getElementById('secretDisplay');
+let resultat = document.getElementsByClassName('resulat')[0];
 
-setInterval(() => {
-    if (s < 59)
-        s++;
-    else {
-        s = 0;
-        if (m < 59)
-            m++;
-        else {
-            m = 0;
-            if (h < 23)
-                h++;
-            else
-                h = 0;
-        }
-    }
-
-    h.length == 1 ? h = `0${h}` : h = `${h}`;
-    m.length == 1 ? m = `0${m}` : m = `${m}`;
-    s.length == 1 ? s = `0${s}` : s = `${s}`;
-
-    resultat.innerHTML = `${h}h: ${m}m: ${s}s`;
-}, 1000);
+myCheckbok.addEventListener('change', () => {
+    if (resultat.hidden == true)
+        resultat.hidden = false;
+    else
+        resultat.hidden = true;
+});
