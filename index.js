2e27f4a96dbfae1969d34465b8f8e2e5b3b96ea1
Maximus203 <printf0cherif@gmail.com>
2022-08-11 21:02:14 +0000

    001-node-exemple

--- index.js
+++
@@ -1,5 +0,0 @@
-function display() {
-    let secret = document.getElementById('theSecret');
-    let resultat = document.getElementById('resulat');
-    resultat.setAttribute('class', 'visually-hidden');
-};
